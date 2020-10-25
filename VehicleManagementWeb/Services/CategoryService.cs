d83db1b984c61a9d4b0e85346aa3349b293a3ffe
Justyna Sobczak <justyna.sobczak@gmail.com>
2020-10-26 11:38:19 +1300

    Add services for Category

---
+++ VehicleManagementWeb/Services/CategoryService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using VehicleManagementModels;
+
+namespace VehicleManagementWeb.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly HttpClient _httpClient;
+        public CategoryService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<Category> AddCategory(Category newCategory)
+        {
+            return await _httpClient.PostJsonAsync<Category>($"api/category", newCategory);
+        }
+
+        public async Task DeleteCategory(int id)
+        {
+            await _httpClient.DeleteAsync($"api/category/{id}");
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _httpClient.GetJsonAsync<Category[]>("api/category");
+        }
+
+        public async Task<Category> GetCategory(int id)
+        {
+            return await _httpClient.GetJsonAsync<Category>($"api/category/{id}");
+        }
+
+        public async Task<Category> UpdateCategory(Category updatedCategory)
+        {
+            return await _httpClient.PutJsonAsync<Category>($"api/category", updatedCategory);
+        }
+    }
+}
---
+++ VehicleManagementWeb/Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehicleManagementModels;
+
+namespace VehicleManagementWeb.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetCategory(int id);
+        Task<Category> UpdateCategory(Category updatedCategory);
+        Task<Category> AddCategory(Category newCategory);
+        Task DeleteCategory(int id);
+    }
+}

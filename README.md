766dccf3091187c260be9a3bc33a88e458a157c2
Justyna Sobczak <justyna.sobczak@gmail.com>
2020-10-26 19:00:58 +1300

    Final touch

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # vehicleManagement
+
+### How to start
+
+1. Open the solution
+2. Check connection string (appsettings.json in VehicleManagementApi project) - if you can use localdb, you don't have to change anything, otherwise change the connection string according to the used db
+3. Go to Visual Studio PowerShell:
+   ```
+   $ Update-Database
+   ```
+4. Click Run (Play button)
+5. Browse: http://localhost:64382
---
+++ VehicleManagementApi/Migrations/20201026055519_NewSeedWithIcons.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VehicleManagementApi.Migrations
+{
+    public partial class NewSeedWithIcons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryName",
+                table: "Categories",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1,
+                column: "Icon",
+                value: "fas fa-motorcycle");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2,
+                column: "Icon",
+                value: "fas fa-car");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3,
+                columns: new[] { "Icon", "WeightUpTo" },
+                values: new object[] { "fas fa-truck", 100000.00m });
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "VehicleId",
+                keyValue: 2,
+                column: "Weight",
+                value: 500.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "VehicleId",
+                keyValue: 3,
+                column: "Weight",
+                value: 5000.00m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryName",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1,
+                column: "Icon",
+                value: "Icon1");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2,
+                column: "Icon",
+                value: "Icon2");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3,
+                columns: new[] { "Icon", "WeightUpTo" },
+                values: new object[] { "Icon3", 5000.00m });
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "VehicleId",
+                keyValue: 2,
+                column: "Weight",
+                value: 10.00m);
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "VehicleId",
+                keyValue: 3,
+                column: "Weight",
+                value: 50.00m);
+        }
+    }
+}
